26377c54f88edb459f6bc6eb66fa7bb830c00f69
Jason <374632897@qq.com>
2017-03-25 06:44:04 +0800

    use normal function insteas of arrow function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function (byte) {
   if (byte < 1024) return byte + ' B';
-  return 'KB, MB, GB, TB'.split(', ').reduce((current, next) => {
+  return 'KB, MB, GB, TB'.split(', ').reduce(function (current, next) {
     if ( typeof current === 'string') return current;
     const format = current / 1024;
     return format <= 1024 ? format.toFixed(2) + ' ' + next : format;
