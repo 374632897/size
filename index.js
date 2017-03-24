e5eb98da2041aaad81db8af3c9252fc93c731055
Jason <374632897@qq.com>
2017-03-24 14:03:33 +0800

    两个小数点

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = function (byte) {
-  if (byte < 1024) return byte + 'B';
-  return 'KB, MB, GB, TB'.split(', ').reduce((pre, next) => {
-    if ( typeof pre === 'string') return pre;
-    const format = pre / 1024;
-    return format <= 1024 ? Math.round(format) + '' +  next : format;
+  if (byte < 1024) return byte + ' B';
+  return 'KB, MB, GB, TB'.split(', ').reduce((current, next) => {
+    if ( typeof current === 'string') return current;
+    const format = current / 1024;
+    return format <= 1024 ? format.toFixed(2) + ' ' + next : format;
   }, byte);
 };
