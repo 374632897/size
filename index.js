baf7f030b34f30a48a5b2514001647347bfa41f6
Jason <374632897@qq.com>
2017-03-25 08:58:08 +0800

    /=

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (byte) {
   if (byte < 1024) return byte + ' B';
   return 'KB, MB, GB, TB'.split(', ').reduce(function (current, next) {
     if ( typeof current === 'string') return current;
-    var format = current / 1024;
-    return format <= 1024 ? format.toFixed(2) + ' ' + next : format;
+    current /= 1024;
+    return current <= 1024 ? current.toFixed(2) + ' ' + next : current;
   }, byte);
 };
