ac52743f21ca33d426a67ff9286d12cf7487a44e
Jason <374632897@qq.com>
2017-03-25 08:56:23 +0800

    use var instead of const

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (byte) {
   if (byte < 1024) return byte + ' B';
   return 'KB, MB, GB, TB'.split(', ').reduce(function (current, next) {
     if ( typeof current === 'string') return current;
-    const format = current / 1024;
+    var format = current / 1024;
     return format <= 1024 ? format.toFixed(2) + ' ' + next : format;
   }, byte);
 };
